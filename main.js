16ffc71304f1dd48801c93a0e01517baf5220591
ShenhElise <100390294+elisenumber1@users.noreply.github.com>
2023-05-07 15:13:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload() {
+}
+
+function setup(){
+  var cnv = createCanvas(500, 500);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  background("gray");
+}
